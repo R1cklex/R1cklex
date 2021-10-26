848e4d27598c1381defbc1ddd2704a3b90cfa04e
R1cklex <52002324+R1cklex@users.noreply.github.com>
2021-10-26 15:42:22 +0300

    Update and rename fibonacci to fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,12 @@
-id = int(input('Enter the number: '))
+def numb():
+    global id
+    id = int(input('Enter the number: '))
+    if id < 0: 
+        print('Wrong Input') 
+        numb()
+    return int(id)
+
+numb()
 
 f0 = 0
 f1 = 1
@@ -6,11 +14,10 @@ i = 2
 
 if id == 0: print(0)
 elif id == 1: print(1)
-elif id > 1:
+else:
     while i <= id:
         f2 = f0 + f1
         f0 = f1
         f1 = f2
         i = i + 1
     print(f2)
-else: print('Wrong Input') 
